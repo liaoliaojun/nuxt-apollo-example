3efade99f6d5ec8fbe7e263ecc4157e327b85ec3
liaoliao <1030219089a@gmail.com>
2020-06-01 01:27:59 +0800

    Refactor: upload feature use hook

---
+++ hooks/upload.ts
@@ -0,0 +1,44 @@
+import {File} from '~/types/index'
+
+// @ts-ignore
+import UPLOAD_FILE from '~/graphql/mutation/upload_file.gql'
+// @ts-ignore
+import UPLOAD_FILE_NETWORK from '~/graphql/mutation/upload_file_network.gql'
+
+export default function useUpload (ctx: any): UseReturn {
+
+  const urlUpload = async (fileUrl: String) => {
+    if (!fileUrl) return null
+
+    const file:File = await ctx.root.$apollo.mutate({
+      mutation: UPLOAD_FILE_NETWORK,
+      variables: {
+        fileUrl,
+        key: localStorage.getItem('app-key'),
+      },
+    }).then((x: any) => x.data.result)
+    return file
+  }
+
+  const localUpload = async ({target}: any) => {
+    if (!target.validity.valid) return null
+    const file:File = await ctx.root.$apollo.mutate({
+      mutation: UPLOAD_FILE,
+      variables: {
+        file: target.files[0],
+        key: localStorage.getItem('app-key'),
+      },
+    }).then((x: any) => x.data.result)
+    return file
+  }
+
+  return {
+    localUpload,
+    urlUpload,
+  }
+}
+
+interface UseReturn {
+  urlUpload (fileUrl: String): Promise<File|null>
+  localUpload ({target}: any): Promise<File|null>
+}
--- pages/upload/index.vue
+++ pages/upload/index.vue
@@ -28,58 +28,33 @@
 <script lang="ts">
   import {defineComponent, ref, Ref} from '@vue/composition-api'
 
-  // @ts-ignore
-  import UPLOAD_FILE from '~/graphql/mutation/upload_file.gql'
-  // @ts-ignore
-  import UPLOAD_FILE_NETWORK from '~/graphql/mutation/upload_file_network.gql'
-
+  import useUpload from '~/hooks/upload'
   import {File} from '~/types/index'
 
   export default defineComponent({
     props: {},
 
     setup (_, ctx: any) {
+      const {urlUpload, localUpload} = useUpload(ctx)
+
       const files: Ref<Array<File>> = ref([])
       const fileUrl: Ref<string> = ref('')
 
       const onUploadImage = async ({target}: any) => {
-        if (!target.validity.valid) return
-        await ctx.root.$apollo.mutate({
-          mutation: UPLOAD_FILE,
-          variables: {
-            file: target.files[0],
-            key: localStorage.getItem('app-key'),
-          },
-        }).then((x: any) => {
-          return x.data.result
-          // keyof T
-          // if (res?.result?.id) {
-          //   files.value.push(data.result || [])
-          // }
-        }).then((file: File) => {
-          files.value.push(file)
+        const data = await localUpload({target})
+        if (data !== null) {
+          files.value.push(data)
           alert('上传本地图片成功')
-        })
+        }
       }
 
-      const onUploadNetwork =  async () => {
-        if (!fileUrl.value) return
-        await ctx.root.$apollo.mutate({
-          mutation: UPLOAD_FILE_NETWORK,
-          variables: {
-            fileUrl: fileUrl.value,
-            key: localStorage.getItem('app-key'),
-          },
-        }).then((x: any) => {
-          return x.data.result
-          // keyof T
-          // if (res?.result?.id) {
-          //   files.value.push(data.result || [])
-          // }
-        }).then((file: File) => {
-          files.value.push(file)
+      
+      const onUploadNetwork = async () => {
+        const data = await urlUpload(fileUrl.value)
+        if (data !== null) {
+          files.value.push(data)
           alert('上传网络图片成功')
-        })
+        }
       }
 
       return {
