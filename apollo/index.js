33cbe1cf66bcf0af9970288a1e68ddc68d5528c8
liaoliaojun <me@liaoliaojun.com>
2020-09-14 14:32:06 +0800

    fix: set apollo defaultOptions
    https://github.com/apollographql/apollo-client/issues/3900

--- apollo/index.js
+++ apollo/index.js
@@ -4,16 +4,20 @@ import fetch from 'node-fetch'
 const defaultClient = new ApolloClient({
   uri: 'https://api.liaoliaojun.com:3000/graphql',
   fetch,
-  // defaultOptions: {
-  //   watchQuery: {
-  //     fetchPolicy: 'network-only',
-  //   },
-  // },
   // httpLinkOptions: {
   //   includeExtensions: true,
   //   credentials: 'include',
   // },
 })
+
+defaultClient.defaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'network-only',
+  },
+  query: {
+    fetchPolicy: 'network-only',
+  }
+}
 
 const apolloClients = {
   default: defaultClient,
