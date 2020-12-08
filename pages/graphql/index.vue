565905ca6f64f2a20cc396c128962b70722f7776
liaoliaojun <me@liaoliaojun.com>
2020-12-08 23:53:09 +0800

    chore: test graphql-codegen

--- pages/graphql/index.vue
+++ pages/graphql/index.vue
@@ -2,31 +2,22 @@
   <div class="px-4 mt-4">
     graphql
     <br>
-    {{ data }}
+    {{ result }}
   </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue'
   import gql from 'graphql-tag'
+  import {useQueryOwnerQuery} from '~/types/schema'
 
   export default Vue.extend({
-    async asyncData ({app}) {
-      const data = await app.apolloProvider.defaultClient.query({
-        query: gql`
-          query getInfo {
-            config {
-              email
-              author
-            }
-          }
-        `,
-      }).then((res: any) => {
-        return res
-      }).catch(() => {
-        return {}
-      })
-      return {data}
+
+    setup () {
+      const {result} = useQueryOwnerQuery()
+      return {
+        result,
+      }
     },
   })
 </script>
